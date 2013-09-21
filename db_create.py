830702abce80a6f2a2d480149229163684e205e5
Philip Daian <phil@linux.com>
2013-09-21 17:46:25 -0500

    update database creation script and instructions

--- db_create.py
+++ db_create.py
@@ -1,7 +1,7 @@
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
-from app import db
+from app import db, models
 import os.path
 
 db.create_all()
@@ -10,3 +10,11 @@ if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+
+user_passwords = raw_input('User default password? : ').strip()
+default_user = models.User(role = models.ROLE_ADMIN, email = 'default')
+default_user.set_password(user_passwords)
+available_user = models.User(role = models.ROLE_USER, email = 'available')
+available_user.set_password(user_passwords)
+
+print 'You may now log in as user default!'
